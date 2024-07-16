fadd1371085be908aba222d6c1b036c217cecf05
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-16 21:21:31 +0200

    funkční doplnit podle posledního comitu

--- library/core/urls.py
+++ library/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('about/', views.about_page, name='about'),
+
 ]
