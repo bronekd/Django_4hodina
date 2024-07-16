dd929816bd14284b12cf88706866b0485362d5ff
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-16 19:17:09 +0200

    model 1

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_data = models.DateField()
+    isbn = models.CharField(max_length=100)
+
+    def __str__(self):
+        return F"{self.title} - {self.author} - {self.isbn}"
+
+
