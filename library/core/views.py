f800ec453ef02bcfb65523a8bef0175bb4e3b142
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-07-16 18:51:04 +0200

    funkční základní aplikace se skládáním stránek

--- library/core/views.py
+++ library/core/views.py
@@ -1,3 +1,15 @@
+
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+
+def home_page(request):
+    return render(request,'pages/home.html')
+
+def about_page(request):
+    return render(request, 'pages/about.html')
+
+
